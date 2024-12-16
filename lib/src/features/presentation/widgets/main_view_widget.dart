757da25d982ae9700abd4331c0f5f1d8c95ea123
Alex <mswd_1@hotmail.com>
2024-12-16 11:51:57 +0200

    Maps added

--- lib/src/features/presentation/widgets/main_view_widget.dart
+++ lib/src/features/presentation/widgets/main_view_widget.dart
@@ -1,4 +1,5 @@
-import 'package:alex_k_test/src/core/exceptions/context.dart';
+import 'dart:async';
+
 import 'package:alex_k_test/src/features/presentation/widgets/general_container.dart';
 import 'package:flutter/material.dart';
 
@@ -47,25 +48,39 @@ class _BodyGradientFusionState extends State<BodyGradientFusion> {
       const Color(0xFF8EA7E9),
     ];
 
-
     int currentPageIndex = _currentPage.floor();
     int nextPageIndex = (currentPageIndex + 1) % pageColors.length;
     double pageProgress = _currentPage - currentPageIndex;
 
     final Color interpolatedBackgroundColor = Color.lerp(
-          pageColors[currentPageIndex],
-          pageColors[nextPageIndex],
-          pageProgress,
-        ) ??
+      pageColors[currentPageIndex],
+      pageColors[nextPageIndex],
+      pageProgress,
+    ) ??
         pageColors[currentPageIndex];
 
     return Container(
-      decoration: BoxDecoration(gradient: LinearGradient(colors: [
-        interpolatedBackgroundColor,
-        const Color(0xFF19A7CE)
-      ])),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [interpolatedBackgroundColor, const Color(0xFF19A7CE)],
+        ),
+      ),
       child: Scaffold(
         appBar: AppBar(
+          leading: currentPageIndex == 1
+              ? IconButton(
+            onPressed: () {
+              unawaited(
+                _pageController.animateToPage(
+                  0,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.ease,
+                ),
+              );
+            },
+            icon: const Icon(Icons.menu),
+          )
+              : null,
           backgroundColor: Colors.transparent,
           title: AnimatedSwitcher(
             duration: const Duration(milliseconds: 500),
@@ -83,15 +98,36 @@ class _BodyGradientFusionState extends State<BodyGradientFusion> {
             },
             child: Text(
               widget.titles[currentPageIndex],
-              key: ValueKey<int>(currentPageIndex), // Ключ для оновлення тайтлу
+              key: ValueKey<int>(currentPageIndex),
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
+          actions: currentPageIndex == 0
+              ? [
+            IconButton(
+              onPressed: () {
+                unawaited(
+                  _pageController.animateToPage(
+                    1,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.ease,
+                  ),
+                );
+              },
+              icon: const Icon(Icons.arrow_forward),
+            ),
+          ]
+              : null,
         ),
         backgroundColor: Colors.transparent,
         body: GeneralContainer(
           child: PageView(
             controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index.toDouble();
+              });
+            },
             children: [
               widget.secondaryView,
               widget.primaryView,
